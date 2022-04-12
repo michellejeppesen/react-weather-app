1db594bb73a44d8336063177a6b8da6e830a091c
Michelle Jeppesen <michellejeppesen@gmail.com>
2022-04-12 05:08:43 -0600

    Deleted unnecessary files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+  <div className="App">
+    Weather App
+    <footer>
+      This project was coded by Michelle Jeppesen and is
+      <a
+        href="https://github.com/michellejeppesen/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+         open-sourced on Github
+      </a>
+    </footer>
+    </div>
+  );
+}
