7e99f02fd9d19b63c2654eef8ff6593e84b81f17
Michelle Jeppesen <michellejeppesen@gmail.com>
2022-04-12 05:17:55 -0600

    Updated links

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./App.css";
 
 export default function App() {
   return (
-  <div className="App">
-    Weather App
-    <footer>
-      This project was coded by Michelle Jeppesen and is
-      <a
-        href="https://github.com/michellejeppesen/react-weather-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-         open-sourced on Github
-      </a>
-    </footer>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href=" " target="_blank" rel="noreferrer"> Michelle Jeppesen</a>{" "}and is{" "}
+        <a
+          href="https://github.com/michellejeppesen/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
