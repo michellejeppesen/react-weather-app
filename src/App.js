eb89f2452f523506c01901cd060cc4f233342918
Michelle Jeppesen <michellejeppesen@gmail.com>
2022-04-13 04:13:40 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href=" " target="_blank" rel="noreferrer"> Michelle Jeppesen</a>{" "}and is{" "}
-        <a
-          href="https://github.com/michellejeppesen/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href=" " target="_blank" rel="noreferrer">
+            {" "}
+            Michelle Jeppesen
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/michellejeppesen/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
